aacafcb1c8be7af456bdc3320d4ed25b3583f10b
SagynalievTaalaibek <sagynalievv.t@gmail.com>
2024-03-25 12:45:30 +0600

    add userSchema

---
+++ src/users/users.controller.ts
@@ -0,0 +1,32 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { User, UserDocument } from '../schemas/user.schema';
+import { Model } from 'mongoose';
+import { RegisterUserDto } from './register-user.dto';
+
+@Controller('users')
+export class UsersController {
+  constructor(
+    @InjectModel(User.name)
+    private userModel: Model<UserDocument>,
+  ) {}
+
+  @Post()
+  async register(@Body() registerUserDto: RegisterUserDto) {
+    try {
+      const user = new this.userModel({
+        email: registerUserDto.email,
+        password: registerUserDto.password,
+        displayName: registerUserDto.displayName,
+      });
+
+      user.generateToken();
+
+      await user.save();
+      return {
+        message: 'Ok',
+        user,
+      };
+    } catch (e) {}
+  }
+}
