b6c7794ac642ebf473ce27ebedd34da80d4bc0d9
SagynalievTaalaibek <sagynalievv.t@gmail.com>
2024-03-25 13:34:53 +0600

    add function to Log in

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,8 +1,10 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Req, UseGuards } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { User, UserDocument } from '../schemas/user.schema';
 import { Model } from 'mongoose';
 import { RegisterUserDto } from './register-user.dto';
+import { Request } from 'express';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('users')
 export class UsersController {
@@ -28,5 +30,14 @@ export class UsersController {
         user,
       };
     } catch (e) {}
+  }
+
+  @UseGuards(AuthGuard('local'))
+  @Post('sessions')
+  async login(@Req() req: Request) {
+    return {
+      message: 'Correct',
+      user: req.user,
+    };
   }
 }
